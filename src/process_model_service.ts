bd245a70567bce2c7e6d4ce8186faf0098ab61dc
ElRaptorus <christian.werner@5minds.de>
2019-05-21 08:59:38 +0200

    :fire: Clean out unhelpful code docs

--- src/process_model_service.ts
+++ src/process_model_service.ts
@@ -120,14 +120,6 @@ export class ProcessModelService implements IProcessModelService {
     this._processDefinitionRepository.deleteProcessDefinitionById(processModelId);
   }
 
-  /**
-   * Takes the xml code of a given ProcessDefinition and tries to parse it.
-   * If the parsing is successful, the xml is assumed to be valid.
-   * Otherwise an error is thrown.
-   *
-   * @param name The name of the ProcessDefinition to validate.
-   * @param xml  The xml code of the ProcessDefinition to validate.
-   */
   private async _validateDefinition(name: string, xml: string): Promise<void> {
 
     let parsedProcessDefinition: Model.Definitions;
@@ -163,14 +155,6 @@ export class ProcessModelService implements IProcessModelService {
     }
   }
 
-  /**
-   * Retrieves a ProcessModel by its ID.
-   * This is achieved by first retrieving a list of ProcessDefinitions and then
-   * looking for the process that has the matching ID.
-   *
-   * @param   processModelId The ID of the ProcessModel to retrieve.
-   * @returns                The retrieved ProcessModel.
-   */
   private async _getProcessModelById(processModelId: string): Promise<Model.Process> {
 
     const processModelList: Array<Model.Process> = await this._getProcessModelList();
@@ -186,13 +170,6 @@ export class ProcessModelService implements IProcessModelService {
     return matchingProcessModel;
   }
 
-  /**
-   * Retrieves a list of all stored ProcessModels.
-   * This is achieved by first retrieving all stored Definitions and then
-   * taking all processes belonging to each definition.
-   *
-   * @returns The retrieved ProcessModels.
-   */
   private async _getProcessModelList(): Promise<Array<Model.Process>> {
 
     const definitions: Array<Model.Definitions> = await this._getDefinitionList();
@@ -206,11 +183,6 @@ export class ProcessModelService implements IProcessModelService {
     return allProcessModels;
   }
 
-  /**
-   * Retrieves a list of all stored ProcessDefinitions.
-   *
-   * @returns The retrieved ProcessDefinitions.
-   */
   private async _getDefinitionList(): Promise<Array<Model.Definitions>> {
 
     const definitionsRaw: Array<ProcessDefinitionFromRepository> = await this._processDefinitionRepository.getProcessDefinitions();
@@ -225,17 +197,6 @@ export class ProcessModelService implements IProcessModelService {
     return definitionsList;
   }
 
-  /**
-   * Performs claim checks for each Element of the given ProcessModel.
-   * If the user does not have access to a Element, that Element is filtered out
-   * from the ProcessModel.
-   *
-   * These Elements include both, Lanes and FlowNodes.
-   *
-   * @param   identity       Contains the Users auth identity.
-   * @param   processModelId The ID of the ProcessModel to filter.
-   * @returns                The filtered ProcessModel.
-   */
   private async _filterInaccessibleProcessModelElements(identity: IIdentity,
                                                         processModel: Model.Process,
                                                        ): Promise<Model.Process> {
@@ -259,15 +220,6 @@ export class ProcessModelService implements IProcessModelService {
     return processModelCopy;
   }
 
-  /**
-   * Performs claim checks for each Lane of the given LaneSet.
-   * If the user does not have access to a Lane, that Element is filtered out
-   * from the LaneSet.
-   *
-   * @param   identity       Contains the Users auth identity.
-   * @param   processModelId The ID of the ProcessModel to filter.
-   * @returns                The filtered ProcessModel.
-   */
   private async _filterOutInaccessibleLanes(laneSet: Model.ProcessElements.LaneSet, identity: IIdentity): Promise<Model.ProcessElements.LaneSet> {
 
     const filteredLaneSet: Model.ProcessElements.LaneSet = clone(laneSet);
@@ -296,15 +248,6 @@ export class ProcessModelService implements IProcessModelService {
     return filteredLaneSet;
   }
 
-  /**
-   * Uses the IAM Service to perform a claim check for the given user and the
-   * given Lane.
-   *
-   * @param   identity Contains the Users identity.
-   * @param   laneName The name of the Lane for which to perform claim check.
-   * @returns          A Promise, which returns "true", if the user does have
-   *                   the claim, or "false", if he does not.
-   */
   private async _checkIfUserCanAccesslane(identity: IIdentity, laneName: string): Promise<boolean> {
     try {
       await this._iamService.ensureHasClaim(identity, laneName);
@@ -315,13 +258,6 @@ export class ProcessModelService implements IProcessModelService {
     }
   }
 
-  /**
-   * Gets a list of FlowNodes that belong to the given LaneSet.
-   *
-   * @param   laneSet   The LaneSet for which to get the FlowNodes.
-   * @param   flowNodes A list of FlowNodes from which to look for matches.
-   * @returns           A list of FlowNodes, which belong to the given LaneSet.
-   */
   private _getFlowNodesForLaneSet(laneSet: Model.ProcessElements.LaneSet, flowNodes: Array<Model.Base.FlowNode>): Array<Model.Base.FlowNode> {
 
     const accessibleFlowNodes: Array<Model.Base.FlowNode> = [];
@@ -360,13 +296,6 @@ export class ProcessModelService implements IProcessModelService {
     return accessibleFlowNodes;
   }
 
-  /**
-   * Checks if the given ProcessModel contains at least one accessible StartEvent.
-   *
-   * @param   processModel The ProcessModel to check.
-   * @returns              True, if at least one acccessible StartEvent exists,
-   *                       otherwise false.
-   */
   private _checkIfProcessModelHasAccessibleStartEvents(processModel: Model.Process): boolean {
 
     // For this check to pass, it is sufficient for the ProcessModel to have at least one accessible start event.
